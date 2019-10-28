a25c47b5e9e832f135a3dd3ef635f8c7717a34b3
jackhe17 <jackhe17@live.com>
2019-10-28 09:58:05 +0800

    固定资产
    固定资产

---
+++ szps-operation/src/main/java/com/szps/web/controller/dev/fixedasset/MachineController.java
@@ -0,0 +1,118 @@
+package com.szps.web.controller.dev.fixedasset;
+
+import java.util.List;
+
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.szps.common.annotation.Log;
+import com.szps.common.core.controller.BaseController;
+import com.szps.common.core.domain.AjaxResult;
+import com.szps.common.core.page.TableDataInfo;
+import com.szps.common.enums.BusinessType;
+import com.szps.framework.util.ShiroUtils;
+import com.szps.system.domain.SysDept;
+import com.szps.system.domain.SysUser;
+import com.szps.web.domain.dev.Fix;
+import com.szps.web.service.dev.IFixService;
+
+@Controller
+@RequestMapping("/op/fixedasset/machine")
+public class MachineController extends BaseController {
+	 @Autowired
+	private IFixService service;
+	
+	private String prefix = "/fixedasset";
+	
+    @RequiresPermissions("fixedasset:machine:view")
+    @GetMapping()
+    public String view()
+    {
+        return prefix + "/machineview";
+    }
+    
+    /**
+     */
+    @RequiresPermissions("fixedasset:machine:view")
+    @PostMapping("/list")
+    @ResponseBody
+    public TableDataInfo list(Fix obj)
+    {
+        startPage();
+        List<Fix> list = service.selectList(obj);
+        return getDataTable(list);
+    }
+    /**
+     * 新增参数配置
+     */
+    @GetMapping("/add")
+    public String add()
+    {
+        return prefix + "/machineadd";
+    }
+    
+
+    /**
+     * 新增保存日报
+     */
+    @RequiresPermissions("fixedasset:machine:add")
+    @Log(title = "添加日报", businessType = BusinessType.INSERT)
+    @PostMapping("/add")
+    @ResponseBody
+    public AjaxResult addSave( Fix obj)
+    {
+    	SysUser user = ShiroUtils.getSysUser();
+    	SysDept sysDept = user.getDept();
+    	obj.setDeptId(sysDept.getDeptId());
+    	obj.setCreateBy(user.getLoginName());
+    	obj.setDelFlag(Fix.DEL_FLAG_NORMAL);
+        return toAjax(service.insert(obj));
+    }
+    
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") Long id, ModelMap mmap)
+    {
+    	Fix obj = service.selectById(id);
+        
+        mmap.put("obj", obj);
+        return prefix + "/machineedit";
+    }
+    
+    
+    /**
+     * 保存
+     */
+    @Log(title = "日报修改", businessType = BusinessType.UPDATE)
+    @RequiresPermissions("fixedasset:machine:edit")
+    @PostMapping("/edit")
+    @ResponseBody
+    public AjaxResult editSave(@Validated Fix obj)
+    {
+    	obj.setUpdateBy(ShiroUtils.getLoginName());
+        return toAjax(service.update(obj));
+    }
+
+    @RequiresPermissions("fixedasset:machine:delete")
+    @Log(title = "日报删除", businessType = BusinessType.DELETE)
+    @PostMapping("/remove")
+    @ResponseBody
+    public AjaxResult remove(String ids)
+    {
+        try
+        {
+            return toAjax(service.deleteByIds(ids));
+        }
+        catch (Exception e)
+        {
+            return error(e.getMessage());
+        }
+    }
+}
---
+++ szps-operation/src/main/java/com/szps/web/controller/dev/fixedasset/OfficeequipmentController.java
@@ -0,0 +1,118 @@
+package com.szps.web.controller.dev.fixedasset;
+
+import java.util.List;
+
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.szps.common.annotation.Log;
+import com.szps.common.core.controller.BaseController;
+import com.szps.common.core.domain.AjaxResult;
+import com.szps.common.core.page.TableDataInfo;
+import com.szps.common.enums.BusinessType;
+import com.szps.framework.util.ShiroUtils;
+import com.szps.system.domain.SysDept;
+import com.szps.system.domain.SysUser;
+import com.szps.web.domain.dev.Fix;
+import com.szps.web.service.dev.IFixService;
+
+@Controller
+@RequestMapping("/op/fixedasset/officeequipment")
+public class OfficeequipmentController extends BaseController {
+	 @Autowired
+	private IFixService service;
+	
+	private String prefix = "/fixedasset";
+	
+    @RequiresPermissions("fixedasset:officeequipment:view")
+    @GetMapping()
+    public String view()
+    {
+        return prefix + "/officeequipmentview";
+    }
+    
+    /**
+     */
+    @RequiresPermissions("fixedasset:officeequipment:view")
+    @PostMapping("/list")
+    @ResponseBody
+    public TableDataInfo list(Fix obj)
+    {
+        startPage();
+        List<Fix> list = service.selectList(obj);
+        return getDataTable(list);
+    }
+    /**
+     * 新增参数配置
+     */
+    @GetMapping("/add")
+    public String add()
+    {
+        return prefix + "/officeequipmentadd";
+    }
+    
+
+    /**
+     * 新增保存日报
+     */
+    @RequiresPermissions("fixedasset:officeequipment:add")
+    @Log(title = "添加日报", businessType = BusinessType.INSERT)
+    @PostMapping("/add")
+    @ResponseBody
+    public AjaxResult addSave( Fix obj)
+    {
+    	SysUser user = ShiroUtils.getSysUser();
+    	SysDept sysDept = user.getDept();
+    	obj.setDeptId(sysDept.getDeptId());
+    	obj.setCreateBy(user.getLoginName());
+    	obj.setDelFlag(Fix.DEL_FLAG_NORMAL);
+        return toAjax(service.insert(obj));
+    }
+    
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") Long id, ModelMap mmap)
+    {
+    	Fix obj = service.selectById(id);
+        
+        mmap.put("obj", obj);
+        return prefix + "/officeequipmentedit";
+    }
+    
+    
+    /**
+     * 保存
+     */
+    @Log(title = "日报修改", businessType = BusinessType.UPDATE)
+    @RequiresPermissions("fixedasset:officeequipment:edit")
+    @PostMapping("/edit")
+    @ResponseBody
+    public AjaxResult editSave(@Validated Fix obj)
+    {
+    	obj.setUpdateBy(ShiroUtils.getLoginName());
+        return toAjax(service.update(obj));
+    }
+
+    @RequiresPermissions("fixedasset:officeequipment:delete")
+    @Log(title = "日报删除", businessType = BusinessType.DELETE)
+    @PostMapping("/remove")
+    @ResponseBody
+    public AjaxResult remove(String ids)
+    {
+        try
+        {
+            return toAjax(service.deleteByIds(ids));
+        }
+        catch (Exception e)
+        {
+            return error(e.getMessage());
+        }
+    }
+}

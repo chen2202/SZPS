986241e684ceb6c6354eb10e93edbaa07fea323b
1608190220 <2423961530@qq.com>
2019-10-28 19:14:13 +0800

    排水户模块

---
+++ szps-operation/src/main/java/com/szps/web/domain/drainage/Street.java
@@ -0,0 +1,18 @@
+package com.szps.web.domain.drainage;
+
+public class Street {
+	private String administrative_name; //行政区名
+	private String street_name; //街道名
+	public String getAdministrative_name() {
+		return administrative_name;
+	}
+	public void setAdministrative_name(String administrative_name) {
+		this.administrative_name = administrative_name;
+	}
+	public String getStreet_name() {
+		return street_name;
+	}
+	public void setStreet_name(String street_name) {
+		this.street_name = street_name;
+	}
+}

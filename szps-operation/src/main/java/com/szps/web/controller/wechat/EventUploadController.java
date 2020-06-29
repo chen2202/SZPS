e5216ec10b5587d980e77c0b993fcb164e01d455
HJRKING <2942362392@qq.com>
2020-06-30 07:33:27 +0800

    6-30

--- szps-operation/src/main/java/com/szps/web/controller/wechat/EventUploadController.java
+++ szps-operation/src/main/java/com/szps/web/controller/wechat/EventUploadController.java
@@ -1,12 +1,23 @@
 package com.szps.web.controller.wechat;
 
 
+import com.szps.web.domain.event.EventSubmit;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "eventUpload")
 public class EventUploadController {
 
+    @PostMapping(value = "/event/list")
+    @ResponseBody
+    public List<EventSubmit> getEventList(){
+
+        return null;
+    }
 
 }
